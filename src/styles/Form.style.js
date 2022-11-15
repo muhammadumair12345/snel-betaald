743e082f898af212c4badc32e6088e232ef62ad6
Muhammad Umair <Muhammad Umair>
2022-11-16 04:17:56 +0500

    updated

--- src/styles/Form.style.js
+++ src/styles/Form.style.js
@@ -1,7 +1,17 @@
 import styled from "@emotion/styled";
 
 export const Form = styled.form`
+  margin-top: 1.5rem;
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) => (props.direction ? props.direction : "column")};
+  flex-wrap: ${(props) => props.wrap};
   gap: 1rem;
+  button {
+    width: 12rem;
+  }
+  @media only screen and (max-width: 550px) {
+    button {
+      width: 100%;
+    }
+  }
 `;
