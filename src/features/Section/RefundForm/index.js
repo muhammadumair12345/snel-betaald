065876ae8a0c1fd7d5607d426b7ead5dd1cc3962
Muhammad Umair <Muhammad Umair>
2022-11-16 13:44:03 +0500

    updated

--- src/features/Section/RefundForm/index.js
+++ src/features/Section/RefundForm/index.js
@@ -5,6 +5,7 @@ import {
   FormControlLabel,
   Link,
   TextField,
+  useMediaQuery,
 } from "@mui/material";
 import { Form } from "../../../styles/Form.style";
 import { Box } from "../../../styles/Box.style";
@@ -20,6 +21,7 @@ const initialData = {
 };
 
 const RefundForm = () => {
+  const matches = useMediaQuery("(max-width: 550px)");
   const [form, setForm] = useState(initialData);
 
   const handleChange = (e) => {
@@ -38,67 +40,78 @@ const RefundForm = () => {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <TextField
-        name="name"
-        label="Your Name"
-        required
-        onChange={handleChange}
-        value={form.name}
-        size="small"
-      />
-      <TextField
-        name="phone"
-        type="tel"
-        label="Phone Number"
-        required
-        onChange={handleChange}
-        value={form.phone}
-        size="small"
-      />
-      <TextField
-        name="email"
-        type="email"
-        label="Email"
-        required
-        onChange={handleChange}
-        value={form.email}
-        size="small"
-      />
+      <Box>
+        <Box wrap="wrap">
+          <TextField
+            fullWidth={matches}
+            name="name"
+            label="Your Name"
+            required
+            onChange={handleChange}
+            value={form.name}
+            size="small"
+          />
+          <TextField
+            fullWidth={matches}
+            name="phone"
+            type="tel"
+            label="Phone Number"
+            required
+            onChange={handleChange}
+            value={form.phone}
+            size="small"
+          />
+          <TextField
+            fullWidth={matches}
+            name="email"
+            type="email"
+            label="Email"
+            required
+            onChange={handleChange}
+            value={form.email}
+            size="small"
+          />
 
-      <TextField
-        name="city"
-        label="City"
-        required
-        onChange={handleChange}
-        value={form.city}
-        size="small"
-      />
-      <TextField
-        name="iban"
-        label="IBAN"
-        required
-        onChange={handleChange}
-        value={form.iban}
-        size="small"
-      />
-      <TextField
-        name="pasnummer"
-        type="number"
-        label="RFID Pasnummer"
-        required
-        onChange={handleChange}
-        value={form.pasnummer}
-        size="small"
-      />
-      <TextField
-        name="cvc"
-        type="number"
-        label="RFID CVC"
-        required
-        onChange={handleChange}
-        value={form.cvc}
-        size="small"
-      />
+          <TextField
+            fullWidth={matches}
+            name="city"
+            label="City"
+            required
+            onChange={handleChange}
+            value={form.city}
+            size="small"
+          />
+          <TextField
+            fullWidth={matches}
+            name="iban"
+            label="IBAN"
+            required
+            onChange={handleChange}
+            value={form.iban}
+            size="small"
+          />
+          <TextField
+            fullWidth={matches}
+            name="pasnummer"
+            type="number"
+            label="RFID Pasnummer"
+            required
+            onChange={handleChange}
+            value={form.pasnummer}
+            size="small"
+          />
+          <TextField
+            fullWidth={matches}
+            name="cvc"
+            type="number"
+            label="RFID CVC"
+            required
+            onChange={handleChange}
+            value={form.cvc}
+            size="small"
+          />
+        </Box>
+      </Box>
       <Box align="center" gap="0">
         <FormControlLabel control={<Checkbox />} />
         <div>
